2dc0edff378167bfca0646744949055b14143c24
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-12-15 12:25:54 +0500

    many to many relationship, one to many relationship

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,12 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
     
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+    
 class Product(models.Model):
     CATEGORY = (('Indoor', 'Indoor'),
                 ('Outdoor', 'Outdoor'))
@@ -18,11 +24,18 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return self.name
     
 class Order(models.Model):
     STATUS = (('Pending', 'Pending'),
               ('Out for Delivery', 'Out for Delivery'),
               ('Delivered', 'Delivered'))
     
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
