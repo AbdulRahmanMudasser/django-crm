4cfc7a5702f2624bc485c8176b4d76272c1ff2d9
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-12-20 11:52:35 +0500

    styling create customer and delete order form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from .models import *
 
-class OrderForm(ModelForm):
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
         
-class CustomerForm(ModelForm):
+class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = '__all__'
+        
+        widgets = {
+            field: forms.TextInput(attrs={'class': 'form-control'}) for field in fields
+        }
+        
