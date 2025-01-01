9a08df6f982cf325cbeee43e89e5e042e126e12f
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-01-01 12:07:05 +0500

    enhanced customer filters

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,13 +1,35 @@
-from django_filters import FilterSet, DateFilter
-from .models import *
+from django_filters import FilterSet, DateFilter, CharFilter
+from django.forms import TextInput, DateInput
+from .models import Order
 
 # Order Filter
 class OrderFilter(FilterSet):
-    start_date = DateFilter(field_name='date_created', lookup_expr='gte', label='Orders Placed After')
-    end_date = DateFilter(field_name='date_created', lookup_expr='lte', label='Orders Placed Before')
-    
+    start_date = DateFilter(
+        field_name='date_created', 
+        lookup_expr='gte', 
+        label='Orders Placed After',
+        widget=DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    end_date = DateFilter(
+        field_name='date_created', 
+        lookup_expr='lte', 
+        label='Orders Placed Before',
+        widget=DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+    product = CharFilter(
+        field_name='product__name',
+        lookup_expr='icontains',
+        label='Product',
+        widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Search Product'})
+    )
+    status = CharFilter(
+        field_name='status',
+        lookup_expr='icontains',
+        label='Status',
+        widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Status'})
+    )
+
     class Meta:
         model = Order
         fields = '__all__'
         exclude = ['customer', 'date_created']
-        
